0486c6e1f1bd32331baf05dc3782536eeeb7693b
Benkeno <81274961+Benkeno@users.noreply.github.com>
2022-01-06 19:51:55 +0100

    06

---
+++ Aufgabeblatt_03.py
@@ -0,0 +1,20 @@
+AnfangsTempKaffee = int(input("Gib die Kaffeetemperatur ein: "))
+Zimmertemperatur = 21
+AbnahmeFaktor = 10
+Abnahme = 0
+NeueTempKaffee = 0
+Minute = 0
+Differenz = AnfangsTempKaffee - Zimmertemperatur
+print("Die Zimmertemperatur beträgt: ", Zimmertemperatur,"° Grad")
+print("Die Differenz beträgt: ", Differenz, "° Grad")
+
+while Differenz > 0.5:
+    Minute = Minute + 1
+    Differenz = Differenz - (Differenz / 100 * AbnahmeFaktor)
+    Abnahme = AnfangsTempKaffee - Differenz - Zimmertemperatur
+    NeueTempKaffee = AnfangsTempKaffee - Abnahme
+
+    
+    print("Minute:", Minute, "; Temperaturabnahme:", round(Abnahme,2), "°; Differenz:",round(Differenz,2), "°; Neue Temperatur:", round(NeueTempKaffee,2),"° Grad")
+    if Differenz < 0.5:
+        print("Kaffee kalt !!")
